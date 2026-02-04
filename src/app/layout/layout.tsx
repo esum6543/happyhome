318847d1542c1e67efec4b54e7798abf8ced9a7d
esum6543 <eldinsumic@gmail.com>
2026-02-04 12:17:55 -0600

    Update layout.tsx

--- src/app/layout/layout.tsx
+++ src/app/layout/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "HappyHomeByEnela",
+  title: "HappyHome ByEnela",
   description: "Home styling • Organization • Photo digitizing in St. Louis.",
 };
 
